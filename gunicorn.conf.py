0caa7090eff9442572f93014fcd3bcc089c81de1
Donald Zou <donaldzou@live.hk>
2025-03-31 19:24:12 +0800

    Commit

---
+++ gunicorn.conf.py
@@ -0,0 +1,10 @@
+from datetime import datetime
+
+date = datetime.today().strftime('%Y_%m_%d_%H_%M_%S')
+pidfile = './gunicorn.pid'
+wsgi_app = "main:app"
+bind = '0.0.0.0:10087'
+accesslog = f"./log/access_{date}.log"
+log_level = "debug"
+capture_output = True
+errorlog = f"./log/error_{date}.log"
