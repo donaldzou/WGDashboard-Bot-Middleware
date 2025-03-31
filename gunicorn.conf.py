f3289f69a0c86e8de5df89eac0c9af2b0cd6d17d
Donald Zou <donaldzou@live.hk>
2025-03-31 19:50:14 +0800

    Update

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -3,7 +3,8 @@ from datetime import datetime
 date = datetime.today().strftime('%Y_%m_%d_%H_%M_%S')
 pidfile = './gunicorn.pid'
 wsgi_app = "main:app"
-bind = '0.0.0.0:10087'
+bind = "0.0.0.0:8080"
+workers = 2
 accesslog = f"./log/access_{date}.log"
 log_level = "debug"
 capture_output = True
